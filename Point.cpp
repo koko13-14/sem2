4dd93fd3962b11d8d6548d8720e57bfa268633a2
Атавин Вячеслав ТКИ-142 <optimusslava2008@mail.ru>
2026-03-24 00:42:31 +0300

    Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -36,3 +36,11 @@ std::istream& operator>>(std::istream& is, Point& p) {
 	}
 	return is;
 }
+
+bool Point::operator == (const Point& other) const {
+	return (this->x == other.x) && (this->y == other.y);
+}
+
+bool Point::operator != (const Point& other) const {
+	return !(*this == other);
+}
